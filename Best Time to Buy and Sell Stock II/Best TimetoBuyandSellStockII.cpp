c5f2b571d43d7501f3e596c42806714c533f92a4
AbhishekTri <88419578+AbhishekTri@users.noreply.github.com>
2022-04-11 18:31:08 +0530

    Create Best TimetoBuyandSellStockII.cpp
    Only include in Profit if there is positive change in Profit. 
    I.E. i>i-1;

---
+++ Best Time to Buy and Sell Stock II/Best TimetoBuyandSellStockII.cpp
@@ -0,0 +1,8 @@
+  int maxProfit(vector<int>& prices) {
+       int maxP=0;
+        for(int i =1;i<prices.size();i++){
+            if(prices[i]>prices[i-1])
+                maxP+=prices[i]-prices[i-1];
+        }
+        return maxP;
+    }
