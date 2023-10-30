9f9dd08d834c7ce6ff670fc83ce140beeae14a2d
Abhishek Tripathi <88419578+AbhishekTri@users.noreply.github.com>
2023-10-30 10:50:14 +0530

    Update SortIntegersByTheNumOf1Bits.cpp

--- SortIntegersByTheNumOf1Bits/SortIntegersByTheNumOf1Bits.cpp
+++ SortIntegersByTheNumOf1Bits/SortIntegersByTheNumOf1Bits.cpp
@@ -1,3 +1,23 @@
+//using inbuilt method: __builtin_popcount(int n) ---- returns no of set bit in number n;
+vector<int> sortByBits(vector<int>& arr) {
+        
+        auto lambda=[&](int a,int b){
+            int count1_a= __builtin_popcount(a);
+            int count1_b= __builtin_popcount(b);
+
+            if(count1_a ==count1_b)
+                return a<b;
+            
+            return count1_a<count1_b;
+        };
+
+        sort(arr.begin(),arr.end(),lambda);
+
+        return arr;
+    }
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
 class Solution {
 public:
 
